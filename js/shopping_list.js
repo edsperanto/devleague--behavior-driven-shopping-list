443847414e6a885418cab5f986e70a1fb09acde3
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-12-10 19:43:53 -1000

    added removeItem method to shoppinglist class

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -12,4 +12,18 @@ class ShoppingList {
     throw new Error('Not a Shopping list item');
     }
   }
+
+  removeItem(shoppinglistitem) {
+    if ( this.items.indexOf(shoppinglistitem) > -1 ) {
+      this.items.splice(this.items.indexOf(shoppinglistitem), 1);
+    } else if ( shoppinglistitem instanceof ShoppingListItem === false ) {
+      throw new Error('Not a Shopping list item');
+    } else {
+      if ( shoppinglistitem === undefined && this.items.length >= 1 ) {
+        this.items.pop();
+      }
+    }
+  }
+
+
 }
