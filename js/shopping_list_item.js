bf5178d12e6f38288a03680ca7b6b8c377dfe6ac
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-12-13 19:26:14 -1000

    not sure

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -15,6 +15,7 @@ class ShoppingListItem {
   }
 
   render() {
-    return `<li class='completed_${this.is_done}'><span>${this.name}</span> <span>${this.description}</span></li>`;
+
+    return `<li class='completed_${this.is_done}'><input type='checkbox'><span>${this.name}</span> <span>${this.description}</span></li>`;
   }
 }
