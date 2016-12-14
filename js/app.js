1388839c9aaf0658e593a22775014fdd2572bb56
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-12-13 21:49:54 -1000

    added event handlers for checkbox

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@ console.log('we good');
 
 const theShoppingList = new ShoppingList(); //instance of ShoppingList class
 
+function changeCheckedStatus(idx, checkbox) {
+  if ( checkbox.checked === true ) {
+    theShoppingList.items[idx].check();
+    console.log('checked');
+  } else {
+    theShoppingList.items[idx].uncheck();
+    console.log('uncheck');
+  }
+}
 
 //function to add things to shopping list and re-render to the browser page
 function addToShoppingList() {
@@ -18,10 +27,11 @@ function addToShoppingList() {
   for ( let i = 0; i < checkBoxNodeList.length; i ++ ) {
     console.log(checkBoxNodeList[i]);
     checkBoxNodeList[i].addEventListener('change', function() {
-      console.log('IM WORKING CHEE!');
+      changeCheckedStatus(i, checkBoxNodeList[i]);
     });
   }
 }
+
 
 //event listener for button to add things to the shopping list
 add_shopping_list_item_button.addEventListener('click', addToShoppingList);
