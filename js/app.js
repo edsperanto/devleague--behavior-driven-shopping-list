0cbf3eacbc7c942248a1230f1b7ecf368cb848e3
Edward Gao <egao@sent.com>
2016-12-14 13:09:09 -1000

    make code more DRY by outsourcing to html tags

--- js/app.js
+++ js/app.js
@@ -1,41 +1,20 @@
 // jshint esversion: 6
-console.log('we good');
-
-const theShoppingList = new ShoppingList(); //instance of ShoppingList class
+const theShoppingList = new ShoppingList();
 
 function changeCheckedStatus(idx, checkbox) {
-  console.log(idx);
   if ( checkbox.checked === true ) {
     theShoppingList.items[idx].check();
-    console.log('checked');
   } else {
     theShoppingList.items[idx].uncheck();
-    console.log('uncheck');
   }
 }
 
 function removeItemButtonClicked(idx) {
-  console.log(theShoppingList.items[idx]);
   theShoppingList.removeItem(theShoppingList.items[idx]);
-
   document.getElementById('content').innerHTML = theShoppingList.render();
-  let checkBoxNodeList = document.querySelectorAll('.check-box');
-    let xButton = document.querySelectorAll('.x-button');
-
-  for ( let i = 0; i < checkBoxNodeList.length; i ++ ) {
-    console.log(checkBoxNodeList[i]);
-    checkBoxNodeList[i].addEventListener('change', function() {
-      changeCheckedStatus(i, checkBoxNodeList[i]);
-    });
-
-    xButton[i].addEventListener('click', function() {
-      removeItemButtonClicked(i);
-    });
-  }
-
+  addDatasetIdxToLiElements();
 }
 
-//function to add things to shopping list and re-render to the browser page
 function addToShoppingList() {
   let item = document.getElementById('item-name').value;
   let description = document.getElementById('item-desc').value;
@@ -43,24 +22,15 @@ function addToShoppingList() {
   theShoppingList.addItem(newShoppingListItem);
   let outPutList = theShoppingList.render();
   document.getElementById('content').innerHTML = outPutList;
-
-  let checkBoxNodeList = document.querySelectorAll('.check-box');
-  let xButton = document.querySelectorAll('.x-button');
-
-  for ( let i = 0; i < checkBoxNodeList.length; i ++ ) {
-    console.log(checkBoxNodeList[i]);
-    checkBoxNodeList[i].addEventListener('change', function() {
-      changeCheckedStatus(i, checkBoxNodeList[i]);
-    });
-
-    xButton[i].addEventListener('click', function() {
-      removeItemButtonClicked(i);
-    });
-  }
-
-
+  addDatasetIdxToLiElements();
 }
 
+function addDatasetIdxToLiElements() {
+  let liElements = document.querySelectorAll('li');
+  for ( let i = 0; i < liElements.length; i ++ ) {
+    liElements[i].dataset.idx = i;
+  }
+}
 
 //event listener for button to add things to the shopping list
 add_shopping_list_item_button.addEventListener('click', addToShoppingList);
