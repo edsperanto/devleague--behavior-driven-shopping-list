63647322dd9f3139b9e0654e21a9b735d5af0bcb
Edward Gao <egao@sent.com>
2016-12-14 19:38:01 -1000

    deconstructed string

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -17,5 +17,27 @@ class ShoppingListItem {
   render() {
     let checking = (this.is_done) ? ("checked = 'checked' ") : ("");
     return `<li class='completed_${this.is_done}'><input class='check-box' type='checkbox' ${checking}onchange="changeCheckedStatus.call(this, this.parentElement.getAttribute('data-idx'), document.querySelectorAll('.check-box')[this.parentElement.getAttribute('data-idx')])")><span>${this.name}</span> <span class='desc'>${this.description}</span><button class='x-button' onclick="removeItemButtonClicked(this.parentElement.getAttribute('data-idx'))">[ x ]</button></li>`;
+    /*
+      deconstructed
+      <li class='completed_${this.is_done}'>
+        <input  class='check-box'
+                type='checkbox'
+                ${checking}
+                onchange=
+                  "changeCheckedStatus
+                    .call(this,
+                          this.parentElement.getAttribute('data-idx'),
+                          document.querySelectorAll('.check-box')
+                            [this.parentElement.getAttribute('data-idx')]
+                          )
+                  ")
+        >
+        <span>${this.name}</span>
+        <span class='desc'>${this.description}</span>
+        <button class='x-button' onclick="removeItemButtonClicked(this.parentElement.getAttribute('data-idx'))">
+          [ x ]
+        </button>
+      </li>
+     */
   }
 }
