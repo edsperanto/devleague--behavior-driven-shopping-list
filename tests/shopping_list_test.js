87953c96f4a16c793df439cdf21a0e35b6550733
Edward Gao <egao@sent.com>
2016-12-14 20:47:33 -1000

    fixed issue 17

--- tests/shopping_list_test.js
+++ tests/shopping_list_test.js
@@ -2,12 +2,16 @@
 
 describe('ShoppingList', () => {
 
-  let myList = new ShoppingList();
+  let myList;
   let newItem1 = new ShoppingListItem('Asparagus Water', 'Magical water that cures cancer from Whole Foods');
   let newItem2 = new ShoppingListItem('Organic Honey', 'Wild Honey from the Amazon');
   let newItem3 = new ShoppingListItem('Tulips', 'Tiptoe through me~');
   let newItem4 = new ShoppingListItem('Gluten free gluten', 'Now with natural ingredients!');
   let newItem5 = new ShoppingListItem('FAKE CHINESE PRODUCT', 'CHING CHONG POTS');
+
+  beforeEach(() => {
+    myList = new ShoppingList();
+  });
 
   it('should be a class', () => {
     expect(ShoppingList).to.be.a('function');
@@ -26,6 +30,7 @@ describe('ShoppingList', () => {
   });
 
   it('should have \'removeItem\' method', () => {
+    myList.addItem(newItem1);
     expect(myList.removeItem).to.be.a('function');
     myList.removeItem(newItem1);
     expect(myList.items.length).to.equal(0);
@@ -38,6 +43,12 @@ describe('ShoppingList', () => {
   });
 
   it('should have \'render\' method', () => {
+    myList.addItem(newItem1);
+    myList.removeItem(newItem1);
+    myList.addItem(newItem2);
+    myList.addItem(newItem3);
+    myList.addItem(newItem4);
+    myList.removeItem();
     expect(myList.render).to.be.a('function');
     expect(myList.render()).to.equal(`<ul>${myList.items[0].render()}${myList.items[1].render()}</ul>`);
   });
